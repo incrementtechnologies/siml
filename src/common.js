cca3708290baabecc1e144b33004cff541a97837
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-02-01 15:32:00 +0800

    update

--- src/common.js
+++ src/common.js
@@ -3,7 +3,7 @@ let APP_EMAIL = 'support@siml.ph'
 export default {
   sidebarMenu: [
     {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Dashboard', icon: 'fa fa-home', path: 'dashboard', flag: false, subMenu: null},
-    {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Menu', icon: 'fa fa-bars', path: 'menus', flag: false, subMenu: null},
+    {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Menu', icon: 'fa fa-bars', path: 'products', flag: false, subMenu: null},
     {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Bookings', icon: 'fa fa-bookmark', path: 'bookings', flag: false, subMenu: null},
     {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Accounts', icon: 'fa fa-users', path: 'accounts', flag: false, subMenu: null}
   ],
