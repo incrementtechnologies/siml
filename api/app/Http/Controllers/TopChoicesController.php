11cd813d865f55edc5b0f2b90839d0aad4d818f6
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-03-25 13:12:26 +0800

    controller spelling

--- api/app/Http/Controllers/TopChoicesController.php
+++ api/app/Http/Controllers/TopChoicesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TopChoicesController extends ApiController
+class TopChoicesController extends APIController
 {
     function __construct(){
         
