c0d67735791df7a8e03eb4da3e6008309343c41d
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-03-26 08:40:53 +0800

    retrieve synqt, top_choices

--- api/app/Http/Controllers/SynqtController.php
+++ api/app/Http/Controllers/SynqtController.php
@@ -9,5 +9,31 @@ class SynqtController extends APIController
 {
     function __construct(){
         $this->model = new Synqt();
+        $this->notRequired = array('title');
     }
+
+    public function create(Request $request){
+        $data = $request->all();
+    	$data['code'] = $this->generateCode();
+    	$this->model = new Synqt();
+    	$this->insertDB($data);
+    	return $this->response();
+    }
+
+    public function generateCode(){
+        $code = 'SY_'.substr(str_shuffle($this->codeSource), 0, 61);
+        $codeExist = Synqt::where('code', '=', $code)->get();
+        if(sizeof($codeExist) > 0){
+          $this->generateCode();
+        }else{
+          return $code;
+        }
+    }
+
+    public function retrieveByParams($column, $value){
+        $result = Synqt::where($column, '=', $value)->where('deleted_at', '=', null)->get();
+
+        return sizeof($result) > 0 ? $result : [];
+    }
+
 }
--- api/app/Http/Controllers/TopChoiceController.php
+++ api/app/Http/Controllers/TopChoiceController.php
@@ -7,7 +7,27 @@ use App\TopChoice;
 
 class TopChoiceController extends APIController
 {
+    public $synqtClass = 'App\Http\Controllers\SynqtController';
+    public $merchantClass = 'Increment\Imarket\Merchant\Http\MerchantController';
+
     function __construct(){
         $this->model = new TopChoice();
+    }
+
+    public function retrieve(Request $request){
+        $data = $request->all();
+        $con = $data['condition'];
+        $result = TopChoice::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])->offset($data['offset'])->limit($data['limit'])->get();
+        if(sizeof($result) > 0){
+            $i = 0;
+            foreach ($result as $key) {
+                $result[$i]['synqt'] = app($this->synqtClass)->retrieveByParams('id', $result[$i]['synqt_id']);
+                $result[$i]['merchant'] = app($this->merchantClass)->getByParams('id', $result[$i]['payload_value']);
+
+                $i++;
+            }
+            $this->response['data'] = $result;
+        }
+        return $this->response();
     }
 }
