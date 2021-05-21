1d9afc328583b3b00184c59d2f5b44bfff5c436f
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-05-21 15:10:15 +0800

    comment_members

---
+++ api/app/Http/Controllers/CommentMemberController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\CommentMember;
+
+class CommentMemberController extends APIController
+{
+    //
+    public function create(Request $request){
+        $data = $request->all();
+        $exist = $this->checkIfExist($data['account_id'], $data['comment_id']);
+        if($exist){
+            $this->model = new CommentMember();
+            $data = CommentMember::where('account_id', '=', $data['account_id'])->where('comment_id', '=', $data['comment_id'])->update(array(
+                'liked' => $data['liked'],
+                'joined' => $data['joined']
+            ));
+            $this->response['data'] = $data;
+        }else{
+            $data = CommentMember::create($data);
+            $this->response['data'] = $data;
+        }
+        return $this->response();
+    }
+
+    public function checkIfExist($accountId, $commentId){
+        $result = CommentMember::where('account_id', '=', $accountId)->where('comment_id', '=', $commentId)->get();
+        return sizeof($result) > 0 ? true : false;
+    }
+}
