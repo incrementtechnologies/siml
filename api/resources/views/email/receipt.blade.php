854865cb156c6acb6ec243fa5d329126aa73e7c5
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-06-02 13:51:30 +0800

    email

--- api/resources/views/email/receipt.blade.php
+++ api/resources/views/email/receipt.blade.php
@@ -1,16 +1,16 @@
 @component('email.header')
 @endcomponent
 <span>
-    <p>Hi, <span style="font-weight: bold;">{{$data['name']}}</span></p>
+    <p>Hi, <span style="font-weight: bold;">{{$data['reservee']}}</span></p>
     <br>
     <!-- <br> -->
-    <p>Your reservation at <span style="font-weight: bold;">{{$data['restaurant']}}</span> has been <span style="font-weight: bold;">{{$data['status']}}</span>.</p>
+    <p>Your reservation at <span style="font-weight: bold;">{{$data['merchant']}}</span> has been <span style="font-weight: bold;">{{$data['status']}}</span>.</p>
     <br>
-    <p>Reservee: <b>{{$data['name']}}</b> </p>
+    <p>Reservee: <b>{{$data['reservee']}}</b> </p>
     <p>Date of Reservation: <b>{{$data['date']}}</b></p>
     <p>No. of Guest: <b>{{$data['number_of_guests']}}</b></p>
     <p>Status: <b>{{$data['status']}}</b></p>
-    <p>Restaurant Name: <b>{{$data['restaurant']}}</b></p>
+    <p>Restaurant Name: <b>{{$data['merchant']}}</b></p>
     <p>Code: <b>{{$data['code']}}</b></p>
     <br>
     <span>Present above information to verify or as your proof of transaction.</span>
